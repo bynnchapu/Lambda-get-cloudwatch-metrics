9bdedc7c82dba7bc23d7ab4a89b12be2e24865fb
Noriki Nakamura <bynn_chapu@tencho-lab.net>
2022-08-14 21:43:52 +0900

    Add complete set of source code.

---
+++ src/get-cloudwatch-metrics.py
@@ -0,0 +1,42 @@
+import json
+import datetime
+import boto3
+
+UNDEFINED_MESSAGE = 'metric is undefined'
+
+def get_cloudwatch_metrics(query_data):
+    client = boto3.client('cloudwatch')
+    response = client.get_metric_data(
+        MetricDataQueries=[
+            {
+                'Id': 'm1',
+                'MetricStat': {
+                    'Metric': {
+                        'Namespace': query_data['Namespace'],
+                        'MetricName': query_data['MetricName'],
+                        'Dimensions': query_data['Dimensions']
+                    },
+                    'Period': query_data['Period'],
+                    'Stat': query_data['Stat']
+                }
+            }
+        ],
+        StartTime=datetime.datetime.now() - datetime.timedelta(minutes=5),
+        EndTime=datetime.datetime.now()
+    )
+
+    if len(response['MetricDataResults'][0]['Values']) != 0:
+        return response['MetricDataResults'][0]['Values'][0]
+    else:
+        return UNDEFINED_MESSAGE
+
+
+def lambda_handler(event, context):
+    metric_value = get_cloudwatch_metrics(event['query_data'])
+    status_code = 200 if metric_value is not UNDEFINED_MESSAGE else 500
+    return_data = {
+        'status_code': status_code,
+        'value': metric_value
+    }
+
+    return json.dumps(return_data)
